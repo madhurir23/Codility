d2cbeb7e7cc9ac4156821b8c1107f704f9a69aba
Madhuri R <madhuri.ravindramohan@gmail.com>
2020-05-06 18:36:34 -0400

    Create CountDistinctSlices.java

---
+++ CountDistinctSlices.java
@@ -0,0 +1,25 @@
+/*
+Count the number of distinct slices (containing only unique numbers). 
+*/
+
+class Solution {
+    public int solution(int M, int[] A) {
+        int end = 0;
+        int start = 0;
+        int count = 0;
+        
+        boolean[] seen = new boolean[M+1];
+        while (start < A.length) {
+            while (end < A.length && !seen[A[end]]) {
+                seen[A[end++]] = true;
+            }
+            
+            count += end - start;
+            if (count > 1000000000) return 1000000000;
+            if(start <= end && start < A.length) seen[A[start++]] = false;
+            
+        }
+        
+        return count;
+    }
+}
