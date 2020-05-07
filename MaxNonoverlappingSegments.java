19f2c0ea2c95a83c713bad1ec47ea19ddce8fc53
Madhuri R <madhuri.ravindramohan@gmail.com>
2020-05-06 23:01:14 -0400

    Create MaxNonoverlappingSegments.java
    https://app.codility.com/programmers/lessons/16-greedy_algorithms/max_nonoverlapping_segments/

---
+++ MaxNonoverlappingSegments.java
@@ -0,0 +1,23 @@
+/*
+Find a maximal set of non-overlapping segments
+*/
+
+class Solution {
+    public int solution(int[] A, int[] B) {
+        int N =  A.length;
+        int count = 0;
+        int i = 0;
+        while (i < N) {
+            count++;
+            int max = B[i];
+            int j = i+1;
+            while (j < N && A[j] <= max) {
+                max = Math.min(max, B[j]);
+                j++;
+            }
+            i = j;
+        }
+        
+        return count;
+    }
+}
